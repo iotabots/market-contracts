48b5f976eeb08bf9a9208221469febd97fac6ccb
hristo-mavrodiev <h.mavrodiev@abv.bg>
2021-11-17 20:46:38 +0200

    allow 0 price listing

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -52,7 +52,7 @@ contract NFTMarket is ReentrancyGuard {
     uint256 tokenId,
     uint256 price
   ) public payable nonReentrant {
-    require(price > 0, "Price must be at least 1 wei");
+    require(price >= 0, "Price must be at least 0 wei");
     require(msg.value == listingPrice, "Price must be equal to listing price");
     require(IERC721(nftContract).ownerOf(tokenId) == msg.sender, "Only owner can create sell order");
 
